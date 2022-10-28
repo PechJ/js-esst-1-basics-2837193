2f61468b72f06e5a0bdf7e2138c576776fce03de
Ralph Steyer <ralph_steyer@gmx.de>
2022-10-28 10:00:57 +0200

    Update fkt1.js
    Falscher Dateiinhalt gewesen. Korrigiert.

--- kap06/lib/js/fkt1.js
+++ kap06/lib/js/fkt1.js
@@ -1,22 +1,5 @@
-"use strict";
-var a1 = new Array();
-var a2 = [];
-var a3 = {};
-console.log(a1);
-console.log(typeof a1);
-console.log(a2);
-console.log(typeof a2);
-console.log(a3);
-console.log(typeof a3);
-var a4 = new Array(1, 2, 3);
-console.log(a4);
-console.log(typeof a4);
-var a5 = [2, 3, 5, 7];
-console.log(a5);
-console.log(typeof a5);
-var a6 = {
-    name: "Meier",
-    vname: "Hans"
-};
-console.log(a6);
-console.log(typeof a6);
+function test(a,b,c){
+	// Aktivität
+	return 0;
+	// unerreichbarer Code
+}
